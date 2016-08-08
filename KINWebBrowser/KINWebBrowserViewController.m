60a9ba49671bb7ff711b4f2ad167749f121fa819
Ben Kelly <ben@appealsoftware.co.uk>
2016-08-08 17:31:39 +0100

    Changed color to gray

--- KINWebBrowser/KINWebBrowserViewController.m
+++ KINWebBrowser/KINWebBrowserViewController.m
@@ -146,7 +146,8 @@ static void *KINWebBrowserContext = &KINWebBrowserContext;
     
     
     self.progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
-    [self.progressView setTrackTintColor:[UIColor colorWithWhite:1.0f alpha:0.0f]];
+    UIColor *grayColor = [UIColor colorWithRed:0.25 green:0.25 blue:0.25 alpha:1.0];
+    [self.progressView setTrackTintColor:grayColor];
     [self.progressView setFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height-self.progressView.frame.size.height, self.view.frame.size.width, self.progressView.frame.size.height)];
     [self.progressView setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin];
 }
